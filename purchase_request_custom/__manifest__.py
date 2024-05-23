7ba6e677ed6e065c31e57dde6740524b8a73492d
franco0310 <francocorvalan2@gmail.com>
2024-05-23 00:56:40 -0300

    modulo purchase request custom, el cual se interviene el metodo make_purchase_order para conservar la cantidad de articulos, la descripcion y la cantidad cuando se crea la orden de compra desde solicitud de compra

---
+++ purchase_request_custom/__manifest__.py
@@ -0,0 +1,18 @@
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    'name': "Purchase Request Custom",
+    'summary': """
+        Module that adds extra customizations required by Making in the Purchase Request Module
+    """,
+    'author': "SalusERP",
+    "maintainers": ["franco0310"],
+    "website": "https://www.saluserp.com/",
+    "license": "AGPL-3",
+    "category": "Project",
+    "version": "17.0.0.0.0",
+    "installable": True,
+    "application": False,
+    'depends': ['base',
+                'purchase_request',
+                ],
+}
