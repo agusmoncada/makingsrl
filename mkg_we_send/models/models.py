2ac69b64ee5e1aa5b282b85c0288bd9a6584f601
franco0310 <francocorvalan2@gmail.com>
2024-05-30 14:36:00 -0300

    agregado de rangos en secuencia de remitos, validacion del rango y correccion en la generacion de la secuencia para que no consuma la numeracion si no se opta por terminar creando el registro

--- mkg_we_send/models/models.py
+++ mkg_we_send/models/models.py
@@ -1,4 +1,5 @@
 from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class MKGWeSend(models.Model):
     _name = 'mkg.we.send'
@@ -12,13 +13,39 @@ class MKGWeSend(models.Model):
 
     @api.model
     def _default_description(self):
-        return self.env['ir.sequence'].next_by_code('mkg.we.send.description') or 'New'
+        sequence = self.env['ir.sequence'].search([('code', '=', 'mkg.we.send.description')], limit=1)
+        if sequence:
+            next_number = sequence.number_next_actual or 'New'
+            sequence_number = int(next_number)
+            
+            if sequence.min_range <= sequence_number <= sequence.max_range:
+                return next_number
+            else:
+                raise ValidationError(f"El número de secuencia {sequence_number} está fuera del rango configurado ({sequence.min_range} - {sequence.max_range}).")
+        return 'New'
 
+    # @api.model # esto se anula porque ya se llama a _default_description en la definicion del campo
+    # def create(self, vals):
+    #     if 'description' not in vals or not vals['description']:
+    #         vals['description'] = self._default_description()
+    #     return super(MKGWeSend, self).create(vals)
+    
     @api.model
     def create(self, vals):
-        if 'description' not in vals or not vals['description']:
-            vals['description'] = self._default_description()
-        return super(MKGWeSend, self).create(vals)
+        # Creamos el registro y capturamos la excepción si falla
+        try:
+            new_record = super(MKGWeSend, self).create(vals)
+        except Exception as e:
+            new_record = False
+            # Aquí puedes manejar la excepción o simplemente pasarla
+            raise e
+
+        # Si el registro se creó correctamente, generamos la secuencia
+        if new_record:
+            sequence = self.env['ir.sequence'].next_by_code('mkg.we.send.description') or 'New'
+            new_record.name = sequence
+
+        return new_record
 
 
 class ProjectTaskExtension(models.Model):
@@ -30,3 +57,15 @@ class AccountInvoiceExtension(models.Model):
     _inherit = 'account.move'
 
     mkg_we_send_ids = fields.Many2many('mkg.we.send', string='Remitos Asociados')
+
+class IrSequence(models.Model):
+    _inherit = 'ir.sequence'
+
+    min_range = fields.Integer(string="Minimum Range", default=1)
+    max_range = fields.Integer(string="Maximum Range", default=1000)
+
+    @api.constrains('min_range', 'max_range')
+    def _check_ranges(self):
+        for record in self:
+            if record.min_range >= record.max_range:
+                raise ValidationError("Minimum range must be less than maximum range.")
